9e2f581f2f6b7f1a3cd773b57986cadf83cd1074
Urbanino <teccolla@gmail.com>
2020-05-17 01:39:56 +0300

    heroku setup2

--- NewsDjango/settings.py
+++ NewsDjango/settings.py
@@ -105,8 +105,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [STATIC_DIR]
+
+STATIC_ROOT = s.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -126,3 +126,5 @@ import dj_database_url
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
